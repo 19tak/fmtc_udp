1be9ac3dd2d5beca0eb3febf7a1f419e70843c99
19tak <84369912+19tak@users.noreply.github.com>
2022-07-01 22:10:00 +0900

    Add files via upload

--- udp_server_ros_pickle.py
+++ udp_server_ros_pickle.py
@@ -6,7 +6,7 @@ import numpy as np
 import rospy
 from geometry_msgs.msg import Point
 
-host = '10.30.18.65'
+host = '10.30.18.10'
 # host = '10.10.0.55'
 # host = '10.10.0.79'
 port = 5000
@@ -17,23 +17,13 @@ def sockrecv(host,port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind((host, port))
 
-    print("-> waiting for connection")
-
-    # pub = rospy.Publisher('UDP_input',Point,queue_size=10)
-    # rospy.init_node('udp_talker',anonymous=True)
-    # rate = rospy.Rate(10)
-
     while True:
         data, address = sock.recvfrom(length)
         if len(data) < 200:
             axes_info = pickle.loads(data)
 
             if axes_info:
-                axis0, axis1, axis2, axis3 = axes_info["axis0"], axes_info["axis1"], axes_info["axis2"], axes_info["axis3"]
-                print("Axis 0 value: ", axis0)
-                print("Axis 1 value: ", axis1)
-                # print("Axis 2 value: ", axis2)
-                print("Axis 3 value: ", axis3)
+                axis0, axis1, axis2, axis3 = float(axes_info["axis0"]), float(axes_info["axis1"]), float(axes_info["axis2"]), float(axes_info["axis3"])
                 return axis0, axis2, axis3
 
 def talker(host,port):
@@ -52,5 +42,6 @@ if __name__ == '__main__':
     try:
         # sockrecv(host,port)
         talker(host,port)
-    except rospy.ROSInterruptException:
+    except rospy.ROSInterruptException as e:
+        print(e)
         pass
